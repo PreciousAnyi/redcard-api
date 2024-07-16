2fcff3e96b3fb1578ee3868a47befd9613277381
Precious <jkpresh@gmail.com>
2024-07-16 23:44:37 +0100

    modified

--- redcard_api/urls.py
+++ redcard_api/urls.py
@@ -3,19 +3,15 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Your API",
+      title="RedCard API",
       default_version='v1',
-      description="API Description",
+      description="API for Exam Infractions accumulator system",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@yourdomain.com"),
+      contact=openapi.Contact(email="preciousifeanyi.api@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
@@ -28,8 +24,4 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-
 ]
